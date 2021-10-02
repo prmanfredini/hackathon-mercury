f35efef07267f3eaff061690e74bac3cd45047d6
Heitor Moura <heitorandrade2001@gmail.com>
2021-10-02 11:36:08 -0300

    consumindo api jogos

---
+++ src/components/TabelaJogos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tabela-jogos">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabelaJogos'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ListaJogos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="lista-jogos">
+    <h1>Lista Jogos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListaJogos'
+}
+</script>
+
+<style scoped>
+
+</style>
