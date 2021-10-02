3582724646228a8168038fed73364e4e06edf5f7
Bruno Riwerson Silva <riwerson@gmail.com>
2021-10-02 11:35:23 -0300

    Criação da tela de animes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import ListaAnimes from '../views/ListaAnimes.vue'
 
 Vue.use(VueRouter)
 
@@ -13,7 +14,12 @@ const routes = [
   {
     path: '/about',
     component: About
-  }
+  },
+  {
+    path: '/lista-de-animes',
+    name: 'Lista de Animes',
+    component: ListaAnimes
+  },
 ]
 
 const router = new VueRouter({
