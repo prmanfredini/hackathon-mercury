de25ccec1989f0c04dd85cf8bc0d4a0e17201cc9
Bruno Riwerson Silva <riwerson@gmail.com>
2021-10-02 13:00:58 -0300

    Alinhamento dos cards

--- src/views/ListaAnimes.vue
+++ src/views/ListaAnimes.vue
@@ -1,32 +1,26 @@
 <template>
- <v-container>
-      <v-row dense>
-        <v-col cols="12"
-        v-for="anime in animesLista" :key="anime.id">
-
-          <v-card
-            color="#385F73"
-            dark
-          >
+  <v-container>
+    <v-row dense>
+      <v-col cols="12" v-for="anime in animesLista" :key="anime.id">
+        <v-card color="#385F73" dark>
           <v-img :src="anime.photo"></v-img>
-            <v-card-title class="text-h5">
-              {{ anime.name }}
-            </v-card-title>
 
-            
-              <div class="genero"> Genero: {{ anime.genre }} </div>
-              
-              <p>adjective</p>
-              <div class="statust">
-                {{ anime.status }}
-              </div>
-            
-            <v-card-subtitle>{{ anime.description }}</v-card-subtitle>
+          <v-card-title class="text-h5">
+            {{ anime.name }}
+          </v-card-title>
 
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+          <div class="ficha">
+            <div>Genero: {{ anime.genre }}</div>
+            <div>{{ anime.status }}</div>
+          </div>
+
+          <v-card-subtitle class="descricao">{{
+            anime.description
+          }}</v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -48,3 +42,14 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+    .ficha {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .descricao {
+        text-align: justify;
+    }
+</style>
